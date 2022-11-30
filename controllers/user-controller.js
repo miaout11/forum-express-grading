57ba27f52d2a241c34d8ded1ee36584c32cf2ce2
miaout11 <miaout.11@gmail.com>
2022-11-30 23:46:15 +0800

    fix edit user function (+1 squashed commits)
    Squashed commits:
    
    [a1a1027] R03-Q5 modify user profile & add comments seed

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs') // 載入 bcrypt
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const db = require('../models')
-const { User } = db
+const { User, Restaurant, Comment } = db
 
 const userController = {
   // sign up
@@ -52,12 +52,15 @@ const userController = {
   getUser: (req, res, next) => {
     return User.findByPk(req.params.id,
       {
-        raw: true,
-        nest: true
+        nest: true,
+        // 拿到關聯的comment，再拿到comment關聯的Rest
+        include: [
+          { model: Comment, include: Restaurant }
+        ]
       })
       .then(user => {
         if (!user) throw new Error('這位使用者不存在!')
-        res.render('users/profile', { user })
+        res.render('users/profile', { user: user.toJSON() })
       })
       .catch(err => next(err))
   },
@@ -74,6 +77,7 @@ const userController = {
     const userId = req.params.id
     const file = req.file
     if (!name) throw new Error('User name is required!')
+    if (req.user.id !== Number(userId)) throw new Error('不能更改其他使用者的資料!')
     return Promise.all([
       User.findByPk(userId),
       imgurFileHandler(file)
